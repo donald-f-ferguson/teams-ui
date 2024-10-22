dedd8d629270b209820f4b9ef20f66c21a70bcb5
Anil <anil.sehgal@onscale.com>
2024-10-22 13:27:43 -0400

    some config fixes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { FormsModule } from '@angular/forms';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  // put all providers here
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    // with this, you should not need to import HttpClientModule in your components
+    provideHttpClient(),
+    importProvidersFrom(FormsModule),
+  ]
 };
